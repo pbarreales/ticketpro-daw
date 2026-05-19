45f98c5ff8f4797c644579a6f960bc33df6d2bb7
Barryy <barry@MacBook-Pro-de-Barryy.local>
2026-05-19 11:45:12 +0200

    Archivos base de Springboot guardados utilizando Maven

---
+++ src/main/java/com/ticketpro/TicketproApplication.java
@@ -0,0 +1,13 @@
+package com.ticketpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketproApplication.class, args);
+	}
+
+}
